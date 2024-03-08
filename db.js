d840595969678849ae2f7177fc75448e69bf619d
Mason Bybee <bybeemason@gmail.com>
2024-03-08 17:31:55 -0600

    added companies and invoices routes

--- db.js
+++ db.js
@@ -1,2 +1,19 @@
 /** Database setup for BizTime. */
 
+const { Client } = require("pg");
+
+let DB_URI;
+
+if (process.env.NODE_ENV === "test") {
+  DB_URI = "postgresql://mason:GoldNova5@localhost/biztime_test";
+} else {
+  DB_URI = "postgresql://mason:GoldNova5@localhost/biztime";
+}
+
+let db = new Client({
+  connectionString: DB_URI,
+});
+
+db.connect();
+
+module.exports = db;
