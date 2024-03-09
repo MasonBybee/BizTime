91de846b0c8fd89aff08c8e6847ab40e5f7de033
Mason Bybee <bybeemason@gmail.com>
2024-03-08 18:59:56 -0600

    slugified names of new companies

--- routes/companies.js
+++ routes/companies.js
@@ -2,6 +2,7 @@ const express = require("express");
 const ExpressError = require("../expressError");
 const router = express.Router();
 const db = require("../db");
+const slugify = require("slugify");
 
 router.get("/", async (req, res, next) => {
   try {
@@ -35,9 +36,17 @@ router.get("/:code", async (req, res, next) => {
 router.post("/", async (req, res, next) => {
   try {
     const { code, name, description } = req.body;
+    const slugifiedCode = slugify(code, {
+      replacement: "",
+      remove: undefined,
+      lower: true,
+      strict: true,
+      locale: "en",
+      trim: true,
+    });
     const results = await db.query(
       "INSERT INTO companies (code, name, description) VALUES ($1, $2, $3) RETURNING code, name, description",
-      [code, name, description]
+      [slugifiedCode, name, description]
     );
     return res.status(201).json({ company: results.rows[0] });
   } catch (e) {
